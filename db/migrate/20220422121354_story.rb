dbb936f7b7b062cb8f5a919e1d2af792e358c9ee
Mehral Qamar <mehral.qamar@devsinc.com>
2022-05-10 00:02:18 +0500

    Added validations to migrations

--- db/migrate/20220422121354_story.rb
+++ db/migrate/20220422121354_story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Migration[5.2]
   def change
     create_table :stories, &:timestamps
-    add_reference :stories, :user, index: true, foreign_key: true
+    add_reference :stories, :user, index: true, foreign_key: true ,null: false
   end
 end
