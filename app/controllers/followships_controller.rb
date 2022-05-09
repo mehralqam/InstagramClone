b25629348a313a3f0c3ce589f2ab7e1ce4fc0545
Mehral Qamar <mehral.qamar@devsinc.com>
2022-05-09 21:28:00 +0500

    User can see the accounts he follows and accounts who follow him with count

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -5,23 +5,23 @@ class FollowshipsController < ApplicationController
   def create
     @followship = current_user.followships.build(:follower_id => params[:follower_id])
     if @followship.save
-      flash[:notice] = "Added follower"
+      flash[:notice] = "Added following"
       redirect_to root_url
     else
-      flash[:error] = "Unable to add follower."
+      flash[:error] = "Unable to add following."
       redirect_to root_url
     end
   end
 
   def index
     @followers= current_user.followers
-
+    @my_followers=Followship.user_specific_followers(current_user)
   end
 
   def destroy
     @followship = Followship.find(params[:id])
     @followship.destroy
-    flash[:notice] = "Removed follower."
+    flash[:notice] = "Removed following."
     redirect_to current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,12 @@ class User < ApplicationRecord
 
   has_many :followships
   has_many :followers , :through => :followships
-  has_many :inverse_followers, :class_name => "Followers", :foreign_key => "follower_id"
-  has_many :inverse_friends, :through => :inverse_follolwings, :source => :user
+  has_many :inverse_followships, :class_name => "Followship", :foreign_key => "follower_id"
+  has_many :inverse_followers, :through => :inverse_follolwships, :source => :user
   has_many :posts, dependent: :destroy
   has_many :stories, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   # ThinkingSphinx::Callbacks.append(self, :behaviours => [:real_time])
-
-
 
 end
