595b29623dd7580e368862de4e295093398c8e73
Mehral Qamar <mehral.qamar@devsinc.com>
2022-05-09 21:28:00 +0500

    User can see the accounts he follows and accounts who follow him with count

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -4,4 +4,6 @@ class Followship < ApplicationRecord
   validates :follower, uniqueness: true
   belongs_to :user
   belongs_to :follower, :class_name => "User"
+
+  scope :user_specific_followers, ->(current_user) { where('follower_id = ?', current_user) }
 end
